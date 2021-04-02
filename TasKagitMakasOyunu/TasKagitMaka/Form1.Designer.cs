f5e79a1c110d8710ae26feb091e14a9b7e1c76f6
Muhammed ceylan <72267252+MuhammedCeylan00@users.noreply.github.com>
2021-04-02 17:05:16 +0300

    Add files via upload

---
+++ TasKagitMakasOyunu/TasKagitMaka/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace TasKagitMaka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(316, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(241, 211);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(614, 22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(260, 216);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.radioButton1.Location = new System.Drawing.Point(80, 244);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(83, 33);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "TAŞ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.radioButton2.Location = new System.Drawing.Point(373, 244);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(108, 33);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "KAĞIT";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.radioButton3.Location = new System.Drawing.Point(676, 244);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(123, 33);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "MAKAS";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(389, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 109);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "OYNA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(1114, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "BİLGİSAYAR:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label2.Location = new System.Drawing.Point(1163, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "........";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.Location = new System.Drawing.Point(1084, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 39);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "SKOR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.Location = new System.Drawing.Point(1114, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "........";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label5.Location = new System.Drawing.Point(1130, 395);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 39);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1462, 745);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ TasKagitMakasOyunu/TasKagitMaka/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TasKagitMaka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int PuanBilgisayar = 0, PuanOyuncu = 0;
+        int sayac = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] dizi = { "tas", "kagit", "makas" };
+            Random random = new Random();
+            int sayi = random.Next(0, 3);
+            string Bilgisayar = dizi[sayi];
+            label2.Text = Bilgisayar;
+            if((PuanOyuncu==3) || (PuanBilgisayar == 3))
+            {
+                sayac++;
+                
+                if (PuanOyuncu > PuanBilgisayar)
+                {
+                    label5.Text = sayac + ". turda kazanan oyuncu";
+                }
+                else if (PuanBilgisayar > PuanOyuncu)
+                {
+                    label5.Text = sayac + ". turda kazanan bilgisayar";
+                }
+                else if (PuanBilgisayar == PuanOyuncu)
+                {
+                    label5.Text = sayac + ". turda berabere";
+                }
+                PuanBilgisayar = 0;
+                PuanOyuncu = 0; 
+            }
+
+            if ((radioButton1.Checked == true) && (Bilgisayar == "tas"))
+            {
+                PuanBilgisayar++;
+                PuanOyuncu++;
+                label4.Text = "bilgisayar:" + PuanBilgisayar + "-oyuncu:" + PuanOyuncu;
+
+            }
+            else if ((radioButton1.Checked == true) && (Bilgisayar == "kagit"))
+            {
+                PuanBilgisayar++;
+                label4.Text = "bilgisayar:" + PuanBilgisayar + "-oyuncu:" + PuanOyuncu;
+            }
+            else if ((radioButton1.Checked == true) && (Bilgisayar == "makas"))
+            {
+                PuanOyuncu++;
+                label4.Text = "bilgisayar:" + PuanBilgisayar + "-oyuncu:" + PuanOyuncu;
+            }
+            else if ((radioButton2.Checked == true) && (Bilgisayar == "tas"))
+            {
+                PuanOyuncu++;
+                label4.Text = "bilgisayar:" + PuanBilgisayar + "-oyuncu:" + PuanOyuncu;
+            }
+            else if ((radioButton2.Checked == true) && (Bilgisayar == "kagit"))
+            {
+                PuanBilgisayar++;
+                PuanOyuncu++;
+                label4.Text = "bilgisayar:" + PuanBilgisayar + "-oyuncu:" + PuanOyuncu;
+            }
+            else if ((radioButton1.Checked == true) && (Bilgisayar == "makas"))
+            {
+                PuanBilgisayar++;
+                label4.Text = "bilgisayar:" + PuanBilgisayar + "-oyuncu:" + PuanOyuncu;
+            }
+            else if ((radioButton3.Checked == true) && (Bilgisayar == "tas"))
+            {
+                PuanBilgisayar++;
+                label4.Text = "bilgisayar:" + PuanBilgisayar + "-oyuncu:" + PuanOyuncu;
+            }
+            else if ((radioButton3.Checked == true) && (Bilgisayar == "kagit"))
+            {
+                PuanOyuncu++;
+                label4.Text = "bilgisayar:" + PuanBilgisayar + "-oyuncu:" + PuanOyuncu;
+            }
+            else if ((radioButton3.Checked == true) && (Bilgisayar == "makas"))
+            {
+                PuanBilgisayar++;
+                PuanOyuncu++;
+                label4.Text = "bilgisayar:" + PuanBilgisayar + "-oyuncu:" + PuanOyuncu;
+            }
+
+        }
+    }
+}
